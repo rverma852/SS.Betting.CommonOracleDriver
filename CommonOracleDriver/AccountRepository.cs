7305fa4a2e473b30eebb0d264870ae94da161ad4
Rajiv Verma <RVerma@sportingsolutions.com>
2017-08-14 12:31:29 +0100

    Initial console app code

---
+++ CommonOracleDriver/AccountRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonOracleDriver
+{
+    public class AccountRepository
+    {
+        public void PrintAccountDetails(string accountId)
+        {
+            OracleCommand command = null;
+            OracleDataReader reader = null;
+
+            try
+            {
+                command = new OracleCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = ACCOUNT_GET_DETAILS,
+                    Connection = (OracleConnection)_dbContext.Connection,
+                    BindByName = true
+
+                };
+                command.Parameters.Add(DbUtility.CreateParameter("p_account_identifier", OracleDbType.Varchar2, accountId));
+                var cursor = command.Parameters.Add(DbUtility.CreateParameter("po_return_cursor", OracleDbType.RefCursor, null, ParameterDirection.Output));
+
+                command.ExecuteNonQuery();
+
+                reader = ((OracleRefCursor)cursor.Value).GetDataReader();
+
+                while (reader.Read())
+                {
+                    account = new AccountDetailResponse();
+                    ExtractAccountDetailFromReader(reader, account);
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    if (!reader.IsClosed) reader.Close();
+                    reader.Dispose();
+                }
+                if (command != null) command.Dispose();
+            }
+        }
+    }
+}
---
+++ CommonOracleDriver/DbUtility.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Oracle.DataAccess.Client;
+
+namespace AccountService.DataAccess
+{
+
+    public static class DbUtility
+    {
+        /// <summary>
+        /// Creates Oracle SP Parameter
+        /// </summary>
+        /// <param name="parametername">Parameter Name</param>
+        /// <param name="type">OracleDb Type</param>
+        /// <param name="value">Parameter Value</param>
+        /// <param name="direction">Default direction - Input</param>
+        /// <returns>OracleParameter type</returns>
+        public static OracleParameter CreateParameter(string parametername, OracleDbType type, object value,
+            ParameterDirection direction = ParameterDirection.Input)
+        {
+            return new OracleParameter
+            {
+                ParameterName = parametername,
+                OracleDbType = type,
+                Direction = direction,
+                Value = value
+            };
+        }
+
+        public static OracleParameter CreateParameter(string parametername, OracleDbType type, object value, int size,
+            ParameterDirection direction = ParameterDirection.Input)
+        {
+            return new OracleParameter
+            {
+                ParameterName = parametername,
+                OracleDbType = type,
+                Direction = direction,
+                Value = value,
+                Size = size
+            };
+        }
+
+        public static short ToDbBool(this bool value)
+        {
+            return value ? (short)1 : (short)0;
+        }
+
+        public static short? ToDbBool(this bool? value)
+        {
+            return value.HasValue ? (value.Value ? (short?)1 : (short?)0) : null;
+        }
+
+        public static bool? ToNullableBool(this object value)
+        {
+            return (value == DBNull.Value)
+                ? new bool?()
+                : ((short)value == 1);
+        }
+
+        public static bool ToBool(this object value)
+        {
+            return ((short)value == 1);
+        }
+        public static string ToString(this object value)
+        {
+            return (value == DBNull.Value)
+                ? null
+                : (string)value;
+        }
+
+        //Parser Delegate
+        public delegate bool TryParseDelegate<T>(string input, out T output);
+
+
+        public static T TryParse<T>(object reader, TryParseDelegate<T> handler) where T : struct
+        {
+            T result;
+            return handler(reader.ToString(), out result) ? result : default(T);
+        }
+
+        public static T? TryParseNullable<T>(object reader, TryParseDelegate<T> handler) where T : struct
+        {
+            T result;
+            return handler(reader.ToString(), out result) ? result : (T?)null;
+        }
+
+        /// <summary>
+        /// Checks whether input can be converted to decimal and returns decimal if yes, otherwise 0
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static decimal GetDecimalValue(object input)
+        {
+            var result = TryParseNullable<decimal>(input, decimal.TryParse);
+
+            return result ?? 0;
+
+        }
+
+        public static string NullToEmptyString(this string input)
+        {
+            if (input == null)
+                return string.Empty;
+            return input;
+        }
+
+        public static TDest? ConvertTo<TSource, TDest>(this TSource? source) where TDest : struct where TSource : struct
+        {
+            if (source == null)
+            {
+                return null;
+            }
+            return (TDest)Convert.ChangeType(source.Value, typeof(TDest));
+        }
+
+
+        public static TDest ConvertTo<TSource, TDest>(this TSource source)
+            where TDest : struct
+            where TSource : struct
+        {
+            return (TDest)Convert.ChangeType(source, typeof(TDest));
+        }
+
+        public static Tag FindTag(IEnumerable<Tag> tagCollection, string key)
+        {
+            var tag = tagCollection.SingleOrDefault(t => string.Equals(t.Key, key, StringComparison.InvariantCultureIgnoreCase)) ??
+                new Tag(key, string.Empty);
+
+            return tag;
+        }
+
+
+    }
+}
